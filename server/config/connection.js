b32a3d1a4b50ef05369beb1dbc51f2a6b8a5f3a2
codenamejason <codenamejason@outlook.com>
2021-01-27 14:36:44 -0500

    add hosted mongo db conn

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb+srv://jromero:JaxCodes@1@cluster0.yyiht.mongodb.net/deep-thoughts-1?retryWrites=true&w=majority',
+  //'mongodb://localhost/deep-thoughts',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
